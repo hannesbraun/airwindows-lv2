03d1534b5215b910ebc2bfb76c3940b06c021ce8
Hannes Braun <hannesbraun@mail.de>
2021-12-04 23:14:38 +0100

    Fix formatting in BitShiftGain

--- src/BitShiftGain/BitShiftGain.c
+++ src/BitShiftGain/BitShiftGain.c
@@ -64,7 +64,7 @@ static void run(LV2_Handle instance, uint32_t sampleFrames)
 	const float* in2 = bitshiftgain->input[1];
 	float* out1 = bitshiftgain->output[0];
 	float* out2 = bitshiftgain->output[1];
-	const int bitshift = (int) *bitshiftgain->bitshift;
+	const int bitshift = (int) * bitshiftgain->bitshift;
 
 	double gain = 1.0;
 	switch (bitshift) {
