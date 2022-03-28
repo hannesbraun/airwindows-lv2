130184321b316c937f51a3edb95671aa88de5731
Hannes Braun <hannesbraun@mail.de>
2022-03-29 00:23:43 +0200

    PurestGain: remove long double precision

--- src/PurestGain/PurestGain.c
+++ src/PurestGain/PurestGain.c
@@ -22,8 +22,8 @@ typedef struct {
 	const float* gain;
 	const float* slowfade;
 
-	long double fpNShapeL;
-	long double fpNShapeR;
+	uint32_t fpdL;
+	uint32_t fpdR;
 	//default stuff
 	double gainchase;
 	double settingchase;
@@ -75,8 +75,10 @@ static void activate(LV2_Handle instance)
 	purestGain->settingchase = -90.0;
 	purestGain->gainBchase = -90.0;
 	purestGain->chasespeed = 350.0;
-	purestGain->fpNShapeL = 0.0;
-	purestGain->fpNShapeR = 0.0;
+	purestGain->fpdL = 1.0;
+	while (purestGain->fpdL < 16386) purestGain->fpdL = rand() * UINT32_MAX;
+	purestGain->fpdR = 1.0;
+	while (purestGain->fpdR < 16386) purestGain->fpdR = rand() * UINT32_MAX;
 }
 
 static void run(LV2_Handle instance, uint32_t sampleFrames)
@@ -118,8 +120,8 @@ static void run(LV2_Handle instance, uint32_t sampleFrames)
 	double outputgain;
 
 
-	long double inputSampleL;
-	long double inputSampleR;
+	double inputSampleL;
+	double inputSampleR;
 
 	while (sampleFrames-- > 0) {
 		targetgain = pow(10.0, purestGain->settingchase / 20.0);
@@ -141,54 +143,8 @@ static void run(LV2_Handle instance, uint32_t sampleFrames)
 
 		inputSampleL = *in1;
 		inputSampleR = *in2;
-		if (inputSampleL < 1.2e-38 && -inputSampleL < 1.2e-38) {
-			static int noisesource = 0;
-			//this declares a variable before anything else is compiled. It won't keep assigning
-			//it to 0 for every sample, it's as if the declaration doesn't exist in this context,
-			//but it lets me add this denormalization fix in a single place rather than updating
-			//it in three different locations. The variable isn't thread-safe but this is only
-			//a random seed and we can share it with whatever.
-			noisesource = noisesource % 1700021;
-			noisesource++;
-			int residue = noisesource * noisesource;
-			residue = residue % 170003;
-			residue *= residue;
-			residue = residue % 17011;
-			residue *= residue;
-			residue = residue % 1709;
-			residue *= residue;
-			residue = residue % 173;
-			residue *= residue;
-			residue = residue % 17;
-			double applyresidue = residue;
-			applyresidue *= 0.00000001;
-			applyresidue *= 0.00000001;
-			inputSampleL = applyresidue;
-		}
-		if (inputSampleR < 1.2e-38 && -inputSampleR < 1.2e-38) {
-			static int noisesource = 0;
-			noisesource = noisesource % 1700021;
-			noisesource++;
-			int residue = noisesource * noisesource;
-			residue = residue % 170003;
-			residue *= residue;
-			residue = residue % 17011;
-			residue *= residue;
-			residue = residue % 1709;
-			residue *= residue;
-			residue = residue % 173;
-			residue *= residue;
-			residue = residue % 17;
-			double applyresidue = residue;
-			applyresidue *= 0.00000001;
-			applyresidue *= 0.00000001;
-			inputSampleR = applyresidue;
-			//this denormalization routine produces a white noise at -300 dB which the noise
-			//shaping will interact with to produce a bipolar output, but the noise is actually
-			//all positive. That should stop any variables from going denormal, and the routine
-			//only kicks in if digital black is input. As a final touch, if you save to 24-bit
-			//the silence will return to being digital black again.
-		}
+		if (fabs(inputSampleL) < 1.18e-23) inputSampleL = purestGain->fpdL * 1.18e-17;
+		if (fabs(inputSampleR) < 1.18e-23) inputSampleR = purestGain->fpdR * 1.18e-17;
 
 		if (1.0 == outputgain) {
 			*out1 = *in1;
@@ -196,17 +152,19 @@ static void run(LV2_Handle instance, uint32_t sampleFrames)
 		} else {
 			inputSampleL *= outputgain;
 			inputSampleR *= outputgain;
-			//stereo 32 bit dither, made small and tidy.
+			//begin 32 bit stereo floating point dither
 			int expon;
 			frexpf((float)inputSampleL, &expon);
-			long double dither = (rand() / (RAND_MAX * 7.737125245533627e+25)) * pow(2, expon + 62);
-			inputSampleL += (dither - purestGain->fpNShapeL);
-			purestGain->fpNShapeL = dither;
+			purestGain->fpdL ^= purestGain->fpdL << 13;
+			purestGain->fpdL ^= purestGain->fpdL >> 17;
+			purestGain->fpdL ^= purestGain->fpdL << 5;
+			inputSampleL += (((double)purestGain->fpdL - (uint32_t)0x7fffffff) * 5.5e-36l * pow(2, expon + 62));
 			frexpf((float)inputSampleR, &expon);
-			dither = (rand() / (RAND_MAX * 7.737125245533627e+25)) * pow(2, expon + 62);
-			inputSampleR += (dither - purestGain->fpNShapeR);
-			purestGain->fpNShapeR = dither;
-			//end 32 bit dither
+			purestGain->fpdR ^= purestGain->fpdR << 13;
+			purestGain->fpdR ^= purestGain->fpdR >> 17;
+			purestGain->fpdR ^= purestGain->fpdR << 5;
+			inputSampleR += (((double)purestGain->fpdR - (uint32_t)0x7fffffff) * 5.5e-36l * pow(2, expon + 62));
+			//end 32 bit stereo floating point dither
 			*out1 = (float) inputSampleL;
 			*out2 = (float) inputSampleR;
 		}
